2a97c1d35d2c8b329612165e1e94e5f8ce8d47d7
David Pinaud <pinaud@cebc.cnrs.fr>
2021-09-02 16:19:59 +0200

    minor changes to comments

--- groupement.r
+++ groupement.r
@@ -1,11 +1,12 @@
 ###############################
-#### a function to group successive TRUE events of a vector with the same group number.
+#### a function to group successive TRUE events of testing a vector with the same group number.
+#### return a vector (same length as test) giving the group number (NA if the result of the test is FALSE)
 
 ####  David Pinaud 2015-07-06 R version 3.2.1 (2015-06-18)
 
 
-grpt <- function(condition) {    
-		a1 <- condition
+grpt <- function(test) {    
+		a1 <- test
 		a2 <- a1
 		a3 <- a1
 		a2[2:length(a1)] <-  a1[1:(length(a1)-1)]  # décalage vers le bas 
@@ -20,7 +21,7 @@ grpt <- function(condition) {
 				     grp[j] <- indice  # numérotation des séquences
 				     indice <- indice + 1
 			     }
-		      if (a1[j]==TRUE & a3[j]==FALSE)  # cas où la séquense est la même
+		      if (a1[j]==TRUE & a3[j]==FALSE)  # cas où la séquence est la même
 		       {
 			       grp[j] <- grp[j-1]
 		       }
